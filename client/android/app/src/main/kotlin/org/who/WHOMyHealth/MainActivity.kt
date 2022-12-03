cf5e0d0d1efb047a25407d7815509a43910d7e54
AhmadKRim <117188893+AhmadKRim@users.noreply.github.com>
2022-12-03 22:12:25 +0200

    Delete client directory

--- client/android/app/src/main/kotlin/org/who/WHOMyHealth/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package org.who.WHOMyHealth
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- client/lib/generated/build.dart
+++
@@ -1,5 +0,0 @@
-class BuildInfo {
-  static const POPULATED = false;
-  static const GIT_SHA = "";
-  static const FLUTTER_VERSION = '''''';
-}
